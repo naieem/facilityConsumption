4fafb5fb99b2ac2c7783930d137d51aaa5b3b02c
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-11-26 19:59:35 +0600

    unnecessary file removed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import Container from "@material-ui/core/Container";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Header from "./header";
 import Filter from "./filter/filter";
-import * as axios from 'axios';
 import Service from "./service";
 import Chart from "./chart";
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Authorization'] = 'Bearer LWgUxt7aMmyQuoEWUxlcUPbfUpRpVM';
+
 
 function App() {
   const [spinnerStatus, setspinnerStatus] = React.useState(false);
