28b41453578c6a5f88e8c195fa35a8ded37e3314
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-11-26 14:11:22 +0600

    observable added for spinner and chart data subscription

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,9 @@
-import { Subject } from 'rxjs';
+import { Subject } from "rxjs";
 
 const spinnerObservable = new Subject();
+const charDataObservable = new Subject();
 const Service = {
-    spinnerObservable:spinnerObservable
-}
+  spinnerObservable: spinnerObservable,
+  charDataObservable: charDataObservable
+};
 export default Service;
