a94b0d00b05506b81aa07928b370090bb0c93b6f
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-11-26 19:02:15 +0600

    service updated

--- src/service/index.js
+++ src/service/index.js
@@ -1,17 +1,46 @@
 import { Subject } from "rxjs";
 import * as axios from "axios";
 
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Authorization'] = 'Bearer LWgUxt7aMmyQuoEWUxlcUPbfUpRpVM';
+
+
+const baseUrl = "https://demo-api.greenely.com/v1";
 const spinnerObservable = new Subject();
 const charDataObservable = new Subject();
 
-function getConsumptionData(facilityId,fDate,tDate,resolution){
-  return axios.get(`https://demo-api.greenely.com/v1/facility/${facilityId}/consumption?from=${fDate}&to=${tDate}&resolution=${resolution}`);
+/**
+ * getting consumption data according to filter value
+ * @param {*} facilityId 
+ * @param {*} fDate 
+ * @param {*} tDate 
+ * @param {*} resolution 
+ */
+function getConsumptionData(facilityId, fDate, tDate, resolution) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(
+        `${baseUrl}/facility/${facilityId}/consumption?from=${fDate}&to=${tDate}&resolution=${resolution}`
+      )
+      .then(response => {
+        if (response.status === 200 && response.data.data && response.data.data.consumption)
+          resolve(response.data.data.consumption);
+        else resolve(false);
+      });
+  });
 }
-function getFacilityId(){
-  return new Promise((resolve,reject)=>{
-    axios.get("https://demo-api.greenely.com/v1/facility").then((response)=>{
-      if (response.status === 200 && response.data.data && response.data.data.length)
-      resolve(response.data.data[0].id);
+/**
+ * 
+ */
+function getFacilityId() {
+  return new Promise((resolve, reject) => {
+    axios.get(`${baseUrl}/facility`).then(response => {
+      if (
+        response.status === 200 &&
+        response.data.data &&
+        response.data.data.length
+      )
+        resolve(response.data.data[0].id);
       else resolve(false);
     });
   });
@@ -19,7 +48,7 @@ function getFacilityId(){
 const Service = {
   spinnerObservable: spinnerObservable,
   charDataObservable: charDataObservable,
-  getConsumptionData:getConsumptionData,
-  getFacilityId:getFacilityId
+  getConsumptionData: getConsumptionData,
+  getFacilityId: getFacilityId
 };
 export default Service;
